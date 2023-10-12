6708b3635604d9601c0920c3775185f387654d9c
Nguyễn Hoàng Vũ <hoangvu22062001@gmail.com>
2023-10-12 14:00:40 +0700

    Refactor project structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline' // giúp nhất quán, đồng bộ giữa các trình duyệt (MUI)
+import App from './App.jsx'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 // import { ThemeProvider } from '@mui/material/styles'
 import theme from './theme'
